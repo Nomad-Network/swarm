573fbb1caaf6fe632421657340ba0ae67f9ebc1c
neutrino2211 <neutrino2211@gmail.com>
2024-10-04 20:41:47 +0100

    Move to Nomad Ethical Technology License

---
+++ src/root.zig
@@ -0,0 +1,7 @@
+//! By convention, root.zig is the root source file when making a library. If
+//! you are making an executable, the convention is to delete this file and
+//! start with main.zig instead.
+const std = @import("std");
+const nomad = @import("nomad");
+
+pub const Instance = struct {};
